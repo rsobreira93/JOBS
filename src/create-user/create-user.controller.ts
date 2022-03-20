fa6c94f31a3ca1c6bf102c7fcd28bafebda1ba6a
rsobreira93 <sobreiraromuloes@gmail.com>
2022-03-20 10:06:35 -0300

    add user controller

--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './create-user-dto';
 
 @Controller('create-user')
-export class CreateUserController {}
+export class CreateUserController {
+  constructor(private mailService: MailerService) {}
+  @Post('/')
+  async createUser(@Body() createUser: CreateUserDTO) {
+    await this.mailService.sendMail({
+      to: createUser.email,
+      from: 'Equipe solamento <solamento@fakemail.com.br>',
+      subject: 'Seja bem-vindo(a)!',
+      text: `Olá ${createUser.name}, seu cadastro na nossa plataforma foi realizado com sucesso. Seja bem-vindo(a)!. `,
+    });
+    return createUser;
+  }
+}
