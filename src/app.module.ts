2ea1a87a43e5d30ddcac04e9ffbe8c2b5d4e1f72
rsobreira93 <sobreiraromuloes@gmail.com>
2022-03-20 10:06:20 -0300

    Add configurations enviroments andd bull e mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { CreateUserController } from './create-user/create-user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CreateUserController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+    }),
+  ],
+  controllers: [CreateUserController],
+  providers: [],
 })
 export class AppModule {}
