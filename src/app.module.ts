b09014d43c5d95fbb62dd9a0e7fea739f57734a9
rsobreira93 <sobreiraromuloes@gmail.com>
2022-03-20 10:47:32 -0300

    add privideres and route queues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { MailerModule } from '@nestjs-modules/mailer';
-import { BullModule } from '@nestjs/bull';
+import { BullModule, InjectQueue } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MiddlewareBuilder } from '@nestjs/core';
+import { Queue } from 'bull';
+import { createBullBoard } from 'bull-board';
 import { CreateUserController } from './create-user/create-user.controller';
-
+import { SendMailConsumer } from './jobs/sendMail-consumer';
+import { SendMailProducerService } from './jobs/sendMail-producer-service';
+import { BullAdapter } from 'bull-board/bullAdapter';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -23,8 +28,19 @@ import { CreateUserController } from './create-user/create-user.controller';
         },
       },
     }),
+    BullModule.registerQueue({
+      name: 'sendMail-queue',
+    }),
   ],
   controllers: [CreateUserController],
-  providers: [],
+  providers: [SendMailProducerService, SendMailConsumer],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(@InjectQueue('sendMail-queue') private sendMailQueue: Queue) {}
+
+  configure(consumer: MiddlewareBuilder) {
+    const { router } = createBullBoard([new BullAdapter(this.sendMailQueue)]);
+
+    consumer.apply(router).forRoutes('/admin/queues');
+  }
+}
